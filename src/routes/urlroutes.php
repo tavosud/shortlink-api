3ab37e6b75bfa60c587f3fd04f06ca3a68cc4fac
Gustavo Cuyutupa <gustavocuyutupa@outlook.com>
2025-09-11 19:26:08 -0500

    routes call namespace fix

--- src/routes/urlroutes.php
+++ src/routes/urlroutes.php
@@ -1,8 +1,8 @@
 <?php
 
     Use Slim\App;
-    use App\controllers\UrlController;
-    use App\middleware\AuthMiddleware;
+    use App\Controllers\UrlController;
+    use App\Middleware\AuthMiddleware;
     
     return function (App $app) {
         $app->post('/shorten', UrlController::class . ':shorten')->add(new AuthMiddleware());
