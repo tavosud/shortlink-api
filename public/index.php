202a2f7131df64b43b0a03b32e508124c12fd799
Gustavo Cuyutupa <gustavocuyutupa@outlook.com>
2025-09-13 19:19:53 -0500

    fix index cors host

--- public/index.php
+++ public/index.php
@@ -31,7 +31,7 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', $_ENV['CORS_HOST'])
+        ->withHeader('Access-Control-Allow-Origin', getenv('CORS_HOST'))
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     });
